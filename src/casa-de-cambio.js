2f68c587f6d0ca22d077ffe0fa5c641dbee57697
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2022-07-20 19:17:31 -0300

    Actualizacion Fecha
    Agrega la fecha de actulaizacion de la API

--- src/casa-de-cambio.js
+++ src/casa-de-cambio.js
@@ -1,34 +1,43 @@
 // https://v6.exchangerate-api.com/v6/5a8beaf470aec3159a3eaecb/latest/USD
+// /history/USD/YEAR/MONTH/DAY
 
+function obtenerCambios(monedaBase = 'ARS', fecha = 'latest'){
 
+  URL_BASE = 'https://v6.exchangerate-api.com/v6/5a8beaf470aec3159a3eaecb';
 
-
-URL_BASE = 'https://v6.exchangerate-api.com/v6/5a8beaf470aec3159a3eaecb/latest';
-
-
-
-function obtenerCambios(monedaBase = 'ARS'){
-  fetch(`${URL_BASE}/${monedaBase}`)
+  fetch(`${URL_BASE}/${fecha}/${monedaBase}`)
     .then((resultado) => resultado.json())
     .then((datos) =>{
       
       recargarPantalla();
+      
       Object.keys(datos.conversion_rates).forEach(monedaBase => {
         tablaAgregarValores(monedaBase, datos.conversion_rates[monedaBase]);
-        desplegableAgregarValores(monedaBase);
+        listadoAgregarValores(monedaBase);
     })
-  
+    
+      agregarFecha(datos.time_last_update_utc);
     });
 }
 
-document.querySelector('#actualizar').onclick = cambiarMonedaTabla;
 
-function cambiarMonedaTabla(){
-  document.querySelector('#actualizar').addEventListener('click', () => {
-    let monedaSeleccionada = document.querySelector('#monedas').value
-    obtenerCambios(monedaSeleccionada)
-  })
+function recargarPantalla(){
 
+  let tabla = document.querySelector('#informacion-tabla')
+    limpiar(tabla)
+    
+    let monedas = document.querySelector('#monedas')
+    limpiar(monedas)
+    
+    document.querySelector('#ultima-actualizacion').textContent = ''
+
+    
+};
+
+function limpiar (padre){
+  while (padre.firstChild){
+      padre.removeChild(padre.firstChild)
+  }
 }
 
 function tablaAgregarValores(monedaBase, valor) {
@@ -45,37 +54,51 @@ function tablaAgregarValores(monedaBase, valor) {
 
   valoresTabla.append(informacionMoneda, informacionCambio);
   informacionTabla.appendChild(valoresTabla);
-}
+};
 
-function desplegableAgregarValores(monedaBase) {
+function listadoAgregarValores(monedaBase) {
   const elegirMoneda = document.querySelector('#monedas');
-  const optionparaMonedas = document.createElement('option'); 
+  const listadoDeMonedas = document.createElement('option'); 
 
-  optionparaMonedas.setAttribute('value', `${monedaBase}`);
-  optionparaMonedas.textContent = `${monedaBase}`;
+  listadoDeMonedas.setAttribute('value', `${monedaBase}`);
+  listadoDeMonedas.textContent = `${monedaBase}`;
 
-  elegirMoneda.appendChild(optionparaMonedas);
+  elegirMoneda.appendChild(listadoDeMonedas);
 }
 
+document.querySelector('#actualizar').onclick = cambiarMonedaTabla;
 
-function recargarPantalla(){
-
-  let tabla = document.querySelector('#informacion-tabla')
-    limpiar(tabla)
+function cambiarMonedaTabla(){
+    let monedaSeleccionada = document.querySelector('#monedas').value
     
-    let monedas = document.querySelector('#monedas')
-    limpiar(monedas)
+    mostarCartelActualizar();
+    obtenerCambios(monedaSeleccionada)
     
-    document.querySelector('#ultima-actualizacion').textContent = ''
+
 }
 
-function limpiar (padre){
-  while (padre.firstChild){
-      padre.removeChild(padre.firstChild)
-  }
+function agregarFecha(fechaDia){
+  document.querySelector('#ultima-actualizacion').textContent = fechaDia;
+
 }
 
+function mostarCartelActualizar(){
+  document.querySelector('#informacion-tabla').innerHTML = 'Cargando...'
+}
+
+ 
 obtenerCambios();
+  
+
+ 
+
+
+
+
+
+
+
+
 
 
 
